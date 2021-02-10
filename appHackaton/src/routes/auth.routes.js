3f0e49ef4a0681ea6fac4020f724d664cfc5d623
Pedro-Paiva <pedro..payva17@gmail.com>
2021-02-10 06:55:35 -0300

    Login/Cadastro

--- appHackaton/src/routes/auth.routes.js
+++ appHackaton/src/routes/auth.routes.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import SignIn from '../pages/SignIn';
+import SignUp from '../pages/SignUp';
 
 const AuthStack = createStackNavigator();
 
 function AuthRoutes(){
     return(
         <AuthStack.Navigator>
-            <AuthStack.Screen name ="SignIn" component = {SignIn}/>
+            <AuthStack.Screen
+            name ="SignIn" 
+            component = {SignIn}
+            options = {{headerShown: false}}
+            />
+            <AuthStack.Screen
+            name ="SignUp" 
+            component = {SignUp}
+            options = {{
+                headerStyle:{}
+            }}
+            />
         </AuthStack.Navigator>
     );
 
