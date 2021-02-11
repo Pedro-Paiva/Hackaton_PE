175573bcca08f5235e15350d80ce376ddc522100
Pedro-Paiva <pedro..payva17@gmail.com>
2021-02-11 10:51:29 -0300

    Correção

--- appHackaton/App.js
+++ appHackaton/App.js
@@ -2,18 +2,15 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { View, StatusBar } from 'react-native';
-import Routes from './src/routes/index';
-import  AuthProvider from './src/contexts/auth';
+
+
 
 console.disableYellowBox = true;
 
 export default function App() {
  return (
    <NavigationContainer>
-     <AuthProvider>
         <StatusBar backgroundColor = '#131313' barStyle = 'light-content'/>
-        <Routes/>
-      </AuthProvider>
    </NavigationContainer>
   );
 }
